01efadbf085c82a470e1515fb9eb171381cb3beb
Tsuyoshi Nakano <tsuyoz.xxx@gmail.com>
2020-11-27 22:26:00 +0900

    コードスタイルを修正

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -5,6 +5,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //This annotationn is not needed in spring boot2
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer{
+public class WebMvcConfig implements WebMvcConfigurer {
 
 }
